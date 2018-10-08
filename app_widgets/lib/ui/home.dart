16361fc4cf016593374a70853f667f9b4b3fbf0a
Chacko <chackop@gmail.com>
2018-10-08 11:01:26 +0100

    added assets

--- app_widgets/lib/ui/home.dart
+++ app_widgets/lib/ui/home.dart
@@ -3,11 +3,111 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(alignment: Alignment.center,color: Colors.blueAccent, child: Text("Food", textDirection: TextDirection.ltr,),
-    // height: 96.0,
-    // width: 192.0,
-    // margin: EdgeInsets.only(left: 50.0),
-    ));
+    return Center(child: Container(
+      alignment: Alignment.center,
+      color: Colors.blueAccent,
+      padding: EdgeInsets.only(top: 30.0, left: 10.0),
+      child: 
+      Column(children: <Widget>[
+                Row(children: <Widget>[
+                Expanded(child: 
+                Text("Seafood", 
+                textDirection: TextDirection.ltr, 
+                style: TextStyle(
+                  fontSize: 30.0, 
+                  decoration: TextDecoration.none, 
+                  fontFamily: 'Roboto', 
+                  fontWeight: FontWeight.normal
+                )
+                ),
+                ),
+                Expanded(child: 
+                Text("Kappa, Meen", 
+                textDirection: TextDirection.ltr, 
+                style: TextStyle(
+                  fontSize: 20.0, 
+                  decoration: TextDecoration.none, 
+                  fontFamily: 'Roboto', 
+                  fontWeight: FontWeight.normal
+                )
+                ),
+                )
+                
+              ],
+              ),
+              Row(children: <Widget>[
+                Expanded(child: 
+                Text("Meat", 
+                textDirection: TextDirection.ltr, 
+                style: TextStyle(
+                  fontSize: 30.0, 
+                  decoration: TextDecoration.none, 
+                  fontFamily: 'Roboto', 
+                  fontWeight: FontWeight.normal
+                )
+                ),
+                ),
+                Expanded(child: 
+                Text("Porrotta, Chicken", 
+                textDirection: TextDirection.ltr, 
+                style: TextStyle(
+                  fontSize: 20.0, 
+                  decoration: TextDecoration.none, 
+                  fontFamily: 'Roboto', 
+                  fontWeight: FontWeight.normal
+                )
+                ),
+                )
+                
+              ],
+              ), 
+              IndianDishWidget(),  
+              OrderButton() 
+
+      ],)
+      
+      
+      
+    )
+    );
   }
   
+}
+
+class IndianDishWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage indishAsset = AssetImage('images/indian-dish.png');
+    Image image = Image(image: indishAsset, width: 400.0, height: 400.0,);
+    return Container(child: image);
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: RaisedButton(
+        child: Text("Order your Dish"),
+        color: Colors.lightGreen,
+        elevation: 5.0,
+        onPressed: () {
+          order(context);
+        },
+      )
+    );
+    return button;
+  }
+}
+          
+void order(BuildContext context) {
+  var alert = AlertDialog(
+    title: Text("Order Completed"),
+    content: Text("Thanks for your order"),
+  );
+  showDialog(
+    context: context,
+    builder: (BuildContext context) => alert
+  );
 }
