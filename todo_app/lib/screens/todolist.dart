eb9fe0a7b32b48267f24b9a9984418e3a80600c7
Chacko <chackop@gmail.com>
2018-10-09 11:21:56 +0100

    navigation

--- todo_app/lib/screens/todolist.dart
+++ todo_app/lib/screens/todolist.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/model/todo.dart';
 import 'package:todo_app/util/dbhelper.dart';
+import 'package:todo_app/screens/tododetail.dart';
 
 class TodoList extends StatefulWidget {
   @override
@@ -22,7 +23,9 @@ class TodoListState extends State {
     return Scaffold(
       body: todoListItems(),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: () {
+          navigateToDetail(Todo('', 3, ''));
+        },
         tooltip: "Add new Todo",
         child: new Icon(Icons.add),
       ),
@@ -45,6 +48,7 @@ class TodoListState extends State {
             subtitle: Text(this.todos[position].date),
             onTap: () {
               debugPrint("Tapped on " + this.todos[position].id.toString());
+              navigateToDetail(this.todos[position]);
             },
           ),
         );
@@ -88,5 +92,12 @@ class TodoListState extends State {
       default:
         return Colors.green;
     }
+  }
+
+  void navigateToDetail(Todo todo) async {
+    bool result = await Navigator.push(context, 
+      MaterialPageRoute(builder: 
+        (context) => TodoDetail(todo)),
+    );
   }
 }
